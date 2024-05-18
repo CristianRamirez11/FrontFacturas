ea662a7c5fa5750fe35287f1361dd134d91cb697
Cristian Ramirez <cdrz01@gmail.com>
2024-05-18 15:37:26 -0500

    add import { CommonModule } from '@angular/common' at component factura

--- src/app/components/factura/factura.component.ts
+++ src/app/components/factura/factura.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+type Factura = Array<{ numeroDetalle: string; cantidad: number; valorTotal: number; nombreCiudad: string; nombreArticulo: string; numeroFactura: number}>;
 
 @Component({
   selector: 'app-factura',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './factura.component.html',
   styleUrl: './factura.component.css'
 })
 export class FacturaComponent implements OnInit{
-  listaFacturas: any[] = [
-    { NumeroDetalle: '123', Cantidad: '12', ValorTotal: '123123', NombreCiudad:'dferferf', NombreArticulo:'23234', NumeroFactura:'123123' },
-    { NumeroDetalle: '123', Cantidad: '12', ValorTotal: '123123', NombreCiudad:'dferferf', NombreArticulo:'23234', NumeroFactura:'123123' },
+  listaFacturas: Factura = [
+    { numeroDetalle: '123', cantidad: 12, valorTotal: 123123, nombreCiudad:'dferferf', nombreArticulo:'23234', numeroFactura:123123 },
+    { numeroDetalle: '123', cantidad: 12, valorTotal: 123123, nombreCiudad:'dferferf', nombreArticulo:'23234', numeroFactura:123123 },
+    { numeroDetalle: '15', cantidad: 6, valorTotal: 90000, nombreCiudad:'Manizales', nombreArticulo:'Mause', numeroFactura:25 },
   ];
 
   constructor() {}
