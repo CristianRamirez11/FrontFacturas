41b3fa1cbc32e87e3503204f1b11a75dd69c3899
Cristian Ramirez <cdrz01@gmail.com>
2024-05-18 15:00:33 -0500

    Configuracion de UIgradientes, add font awesome y creacion de vista para listar facturas

--- src/app/components/factura/factura.component.ts
+++ src/app/components/factura/factura.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-factura',
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './factura.component.html',
   styleUrl: './factura.component.css'
 })
-export class FacturaComponent {
+export class FacturaComponent implements OnInit{
+  listaFacturas: any[] = [
+    { NumeroDetalle: '123', Cantidad: '12', ValorTotal: '123123', NombreCiudad:'dferferf', NombreArticulo:'23234', NumeroFactura:'123123' },
+    { NumeroDetalle: '123', Cantidad: '12', ValorTotal: '123123', NombreCiudad:'dferferf', NombreArticulo:'23234', NumeroFactura:'123123' },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void{
+
+  }
 }
