c80da5c384549145d07e5c359da597107a3caade
Cristian Ramirez <cdrz01@gmail.com>
2024-05-19 23:35:05 -0500

    se agrega la libreria de ngx-toastr/toastr.css

--- src/app/components/factura/factura.component.ts
+++ src/app/components/factura/factura.component.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ToastrService } from 'ngx-toastr';
+
 type Factura = Array<{ numeroDetalle: string; cantidad: number; valorTotal: number; nombreCiudad: string; nombreArticulo: string; numeroFactura: number}>;
 
 @Component({
   selector: 'app-factura',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, BrowserAnimationsModule, ToastrModule],
   templateUrl: './factura.component.html',
   styleUrl: './factura.component.css'
 })
@@ -22,7 +26,9 @@ export class FacturaComponent implements OnInit{
   form: FormGroup;
   id: number | undefined;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private toastr: ToastrService,
+    private _tarjetaService: TarjetaService) {
     this.form = this.fb.group({
       fecha: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(10)]],
       cliente: ['', Validators.required],
@@ -43,10 +49,10 @@ export class FacturaComponent implements OnInit{
       cantidad: this.form.get('cantidad')?.value,
       articulo: this.form.get('articulo')?.value,
     }
-/*
+
     if(this.id == undefined) {
       // Agregamos una nueva tarjeta
-        this._tarjetaService.saveTarjeta(tarjeta).subscribe(data => {
+        this._tarjetaService.saveTarjeta(factura).subscribe(data => {
           this.toastr.success('La tarjeta fue registrada con exito!', 'Tarjeta Registrada');
           this.obtenerTarjetas();
           this.form.reset();
@@ -62,14 +68,14 @@ export class FacturaComponent implements OnInit{
         this.form.reset();
         this.accion = 'Agregar';
         this.id = undefined;
-        this.toastr.info('La tarjeta fue actualizada con exito!', 'Tarjeta Actualizada');
+        this.toastr.info('La factura fue agregada con exito!', 'Factura ingresada');
         this.obtenerTarjetas();
       }, error => {
         console.log(error);
       })
 
     }
-*/
+
    
   }
 
