6bab0029c4cd9ebcfb8ae610dd8657426040e364
Cristian Ramirez <cdrz01@gmail.com>
2024-05-19 22:31:22 -0500

    Update styles and add fonts.googleapis.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FacturaComponent } from './components/factura/factura.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FacturaComponent],
+  imports: [RouterOutlet, FacturaComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
